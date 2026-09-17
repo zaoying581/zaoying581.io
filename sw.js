198ec1b60f1f1cf9bd1860ee787c96af1511e8ee
zaoying581 <zaoying581@users.noreply.github.com>
2026-09-17 18:27:53 +0800

    update: 最新构建版本

--- sw.js
+++ sw.js
@@ -1,16 +1,23 @@
-const CACHE = 'class-diary-v3';
-const URLS = ['index.html', 'manifest.json'];
+const CACHE = 'study-assistant-v2';
 
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(URLS); }));
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(['/']))
+  );
 });
 
-self.addEventListener('activate', function(e) {
-  e.waitUntil(caches.keys().then(function(keys) {
-    return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
-  }));
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
+      )
+    )
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-  e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
 });
