468d2733860fdac58c073944c7ebea7f2d292efd
zaoying581 <zaoying581@users.noreply.github.com>
2026-09-19 18:45:45 +0800

    feat: 基于新版项目改名为Mikou+加装饰图和水印

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
-const CACHE = 'study-assistant-v3';
+const CACHE = 'study-assistant-v2';
 
 self.addEventListener('install', (e) => {
-  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE).then((cache) => cache.addAll(['/']))
   );
@@ -13,23 +12,11 @@ self.addEventListener('activate', (e) => {
       Promise.all(
         keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
       )
-    ).then(() => self.clients.claim())
+    )
   );
 });
 
 self.addEventListener('fetch', (e) => {
-  // HTML文件始终优先网络，确保拿到最新版本
-  if (e.request.headers.get('accept')?.includes('text/html')) {
-    e.respondWith(
-      fetch(e.request).then((response) => {
-        const copy = response.clone();
-        caches.open(CACHE).then((cache) => cache.put(e.request, copy));
-        return response;
-      }).catch(() => caches.match(e.request))
-    );
-    return;
-  }
-  // 其他静态资源用缓存优先
   e.respondWith(
     caches.match(e.request).then((r) => r || fetch(e.request))
   );
